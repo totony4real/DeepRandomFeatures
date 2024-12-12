94e3e0e541c9c1aacea335e3fe1471876b6827e3
So Takao <sotakao@Sos-MacBook-Pro-3.local>
2024-12-11 18:06:49 -0800

    fixed requirements and setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="."),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
 )
